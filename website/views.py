ad8906f3d8037bf5e25382d9cc774196d0a1d67e
FarmerKarwer <83603595+FarmerKarwer@users.noreply.github.com>
2022-04-17 16:49:51 +0300

    Adding the server

---
+++ website/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home_view(request, *args, **kwargs):
+	return render(request, "index.html", {})
+
+def tests(request, *args, **kwargs):
+	return render(request, "tests.html", {})
+
+def tests_swl(request, *args, **kwargs):
+	context = {
+	"title" : "Удовлетворенность жизнью"
+	}
+	return render(request, "swl.html", context)
+
+def tests_big5(request, *args, **kwargs):
+	context = {
+	"title" : "Big Five"
+	}
+	return render(request, "big five.html", context)
+
+def tests_temper(request, *args, **kwargs):
+	context = {
+	"title" : "Темперамент"
+	}
+	return render(request, "temper.html", context)
+
+def tests_typeofthinking(request, *args, **kwargs):
+	context = {
+	"title" : "Тип мышления"
+	}
+	return render(request, "type_of_thinking.html", context)
