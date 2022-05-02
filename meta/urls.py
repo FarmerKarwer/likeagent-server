554737b26d97a02293579bc5c5ec90487b7a91e6
FarmerKarwer <83603595+FarmerKarwer@users.noreply.github.com>
2022-05-02 21:33:48 +0300

    Almost added views for test results

--- meta/urls.py
+++ meta/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('tests/temper/', tests_temper),
     path('tests/typeofthinking/', tests_typeofthinking),
     path('tests/<pk>/', quiz_view, name='quiz-view'),
+    path('tests/<pk>/save/', save_quiz_view, name='save-view'),
     path('tests/<pk>/data/', quiz_data_view, name='quiz-data-view'),
     path('accounts/', include('website.urls')),
 ]
--- website/views.py
+++ website/views.py
@@ -4,6 +4,11 @@ from django.views.generic import ListView
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.http import JsonResponse
+from django.shortcuts import HttpResponse
+
+
+def is_ajax(request):
+  return request.headers.get('x-requested-with') == 'XMLHttpRequest'
 
 # Create your views here.
 @method_decorator(login_required(login_url='/accounts/vk/login/'), name='dispatch')
@@ -58,3 +63,44 @@ def quiz_data_view(request, pk):
 	return JsonResponse({
 		'data' : questions,
 		})
+
+def save_quiz_view(request, pk):
+	if is_ajax(request):
+		questions = []
+		data = request.POST
+		data_ = dict(data.lists())
+		
+		data_.pop('csrfmiddlewaretoken')
+
+		for k in data_.keys():
+			question = Question.objects.get(text = k)
+			questions.append(question)
+
+		user = request.user
+		quiz = Quiz.objects.get(pk=pk)
+		result_groups = list(Quiz(pk=pk).get_resultgroups())
+
+		for group in result_groups:
+			group_questions = Question.objects.filter(result_group=group)
+			group_results = Result.objects.filter(result_group=group)
+			score = 0
+
+			for q in group_questions:
+				a_selected = request.POST.get(q.text)
+
+				if a_selected != "":
+					question_answers = Answer.objects.filter(question=q)
+					for a in question_answers:
+						if a_selected == a.text:
+							score+=a.score
+			results = []
+			for r in group_results:
+				result_conditions = Condition.objects.filter(result=r)
+				cond_results = []
+				for c in result_conditions:
+					if c.min_score <= score <= c.max_score:
+						cond_results.append({"name":c.name, "text": c.text})
+				results.append({"condition": cond_results, "score": score, "description" : r.text})
+			print(results)
+
+	return JsonResponse({'text': 'works'})
